03ff3b70b88501379cc9c920b0a980c71071f0ef
gharraph <sherief@gharraph.com>
2015-09-14 13:57:05 -0400

    cleaned unnecessary generated functionality

--- Lab5/app/src/main/java/com/ohiohealth/lab5/MainActivity.java
+++ Lab5/app/src/main/java/com/ohiohealth/lab5/MainActivity.java
@@ -1,17 +1,11 @@
 package com.ohiohealth.lab5;
 
-import android.app.Activity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity
@@ -93,21 +87,6 @@ public class MainActivity extends AppCompatActivity
             return true;
         }
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
